c83ba9904cc4ab52df969d1dec7def03519924f9
DebadityaPal <debaditya.pal6@gmail.com>
2021-06-07 18:24:56 +0530

    Documentation Pipeline Added

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Debaditya Pal'
 author = 'Debaditya Pal'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1 alpha'
+release = 'latest'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +31,7 @@ release = '0.1 alpha'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'myst_parser'
+  'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
